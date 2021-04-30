a019ea1543b6dc1515ecd2eda961cdcbd1da8ef5
sudheerbd <32688763+sudheerbd@users.noreply.github.com>
2021-05-01 00:40:44 +0530

    Update main.tf

--- terraform/tf_environment/main.tf
+++ terraform/tf_environment/main.tf
@@ -5,11 +5,22 @@ provider "aws" {
 }
 
 
-resource "aws_ebs_volume" "example" {
-  availability_zone = "us-west-2a"
-  size              = 20
-}
+# resource "aws_ebs_volume" "example" {
+#   availability_zone = "us-west-2a"
+#   size              = 20
+# }
 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
 # module "fss" {
 #   source              = "../modules/file_system"
 #   compartment_ocid    = var.compartment_ocid
