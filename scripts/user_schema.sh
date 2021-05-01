cbd815ed314c2c3feeca12525f36547003893929
sudheerbd <32688763+sudheerbd@users.noreply.github.com>
2021-05-02 01:19:37 +0530

    Update user_schema.sh

--- scripts/user_schema.sh
+++ scripts/user_schema.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . ./scripts/env_variables.sh
 
-# username=${INSTANCE_NAME}
-# password="${username:0:3}Orbit#1234"
+username=${INSTANCE_NAME}
+password="${username:0:3}Orbit#1234"
 
 # sqlplus command to create user schema in oci ADW.
 # sqlplus $sql_username/$sql_pwd@$sql_adw_level << !
