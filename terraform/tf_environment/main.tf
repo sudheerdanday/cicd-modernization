a51d0dc3925ba536facf82bd8c25104184f6b72f
sudheerbd <32688763+sudheerbd@users.noreply.github.com>
2021-05-02 01:05:12 +0530

    Update main.tf

--- terraform/tf_environment/main.tf
+++ terraform/tf_environment/main.tf
@@ -10,17 +10,17 @@ provider "aws" {
 #   size              = 20
 # }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-}
+# resource "aws_db_instance" "default" {
+#   allocated_storage    = 10
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   name                 = "mydb"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot  = true
+# }
 # module "fss" {
 #   source              = "../modules/file_system"
 #   compartment_ocid    = var.compartment_ocid
